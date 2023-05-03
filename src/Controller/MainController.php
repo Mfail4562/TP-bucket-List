224552dca3cb9a27aac228bbb0c1cedf874f471d
mathieu <mathieu.perin.prog@gmail.com>
2023-05-03 13:49:07 +0200

    tp module 4 + tp bonus - mise en place des routes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
     namespace App\Controller;
 
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
     class MainController extends AbstractController
@@ -11,7 +12,21 @@
         /**
          * @Route("/", name="main_home")
          */
-        public function home() {
-        return $this->render('main/home.html.twig');
-    }
+        public function home(): Response
+        {
+            return $this->render('main/home.html.twig');
+        }
+
+        /**
+         * @Route("/about_us", name="main_about_us")
+         */
+        public function about_us(): Response
+        {
+            $file = file_get_contents('../data/team.json');
+            $teamMembers = json_decode($file, true);
+            dump($teamMembers);
+            return $this->render('main/about_us.html.twig', [
+                'teamMembers' => $teamMembers
+            ]);
+        }
     }
