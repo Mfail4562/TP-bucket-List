1caf9dfda96a71f17f6a9f392da98a8ca6b7391f
mathieu <mathieu.perin.prog@gmail.com>
2023-05-04 15:38:31 +0200

    tp module 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
     namespace App\Controller;
 
+    use App\Repository\WishRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +11,25 @@
     class WishController extends AbstractController
     {
         #[Route('/list', name: 'list')]
-        public function list(): Response
+        public function list(WishRepository $wishRepository): Response
         {
-            return $this->render('wish/list.html.twig');
+            $wishes = $wishRepository->findBy(['isPublished' => true], ['dateCreated' => 'DESC']);
+            return $this->render('wish/list.html.twig', [
+                'wishes' => $wishes
+            ]);
         }
 
         #[Route('/details/{id}', name: 'details', requirements: ['id' => "\d+"])]
-        public function details($id): Response
+        public function details($id, WishRepository $wishRepository): Response
         {
-            return $this->render('wish/details.html.twig');
+            $wish = $wishRepository->find($id);
+
+            if (!$wish) {
+                throw $this->createNotFoundException("This wish do not exists! Sorry!");
+            }
+
+            return $this->render('wish/details.html.twig', [
+                'wish' => $wish
+            ]);
         }
     }
